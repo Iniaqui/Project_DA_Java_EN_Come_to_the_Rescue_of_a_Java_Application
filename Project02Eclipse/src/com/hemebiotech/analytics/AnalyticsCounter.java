07f7cb2127e9d859c9ce0a565636b58c32ba4b31
Maurel CODO <maurelcodo@gmail.com>
2020-07-07 19:38:30 +0200

    Version 4 , suppression de la class symptom et creation de la classe symptomHandler pour traiter de fichier comptage des occurences , trie par ordre

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -18,102 +18,30 @@ public class AnalyticsCounter {
 			
 	
 	public static void main(String args[]) throws Exception {
-		/**
-		 * Contains the path of the file which the list of symptom
-		 */
+	//
+		  
+		 
 		//Set the path of File
-		String filepath ="symptoms.txt";
+		
 		/**
 		 * @see ReadSymptomDataFromFile
 		 */
-		ReadSymptomDataFromFile readFile = new ReadSymptomDataFromFile(filepath);
+		SymptomHandler handler=new SymptomHandler();
 		
-		ArrayList<String> ListSymptoms = readFile.GetSymptoms();
+		handler.setSymptomtList();
+		handler.updateSymptom();
+		handler.affiche();
 		/**
 		 * List of the symptom with its characterize and methods
 		 * @see Symptom
 		 */
-		ArrayList<Symptom> Symptom = new ArrayList<Symptom>();
+
 		
-		if(ListSymptoms != null) {//Verify if the list of symptom are not empty  
-			/**
-			 * We take the first symptom of the list of symptom in the Symptom for avoid the exception in order to have a arrayList Symptom not empty
-			 */
-			String premierSymp;
-			premierSymp =ListSymptoms.get(0);
-			Symptom.add(new Symptom(premierSymp));
-			int i = 1, j=0;
-			/**
-			 * To verify if this symptom is already count or not
-			 */
-			boolean exist;
-			/**
-			 * Read every elements of ListSymptoms
-			 */
-			String Line;
-			do {
-				Line = ListSymptoms.get(i);
-				
-				do {
-					
-					if(Line.equals(Symptom.get(j).getName())) {//Verify the correpondance 
-						Symptom.get(j).Counter();
-						exist =true;
-						break;
-					}
-					else {
-						exist =false;
-					}
-					j++;
-					
-				}while(j< Symptom.size() && exist ==false);
-				if(exist== false) {
-					
-					Symptom.add(new Symptom(Line));//add this symptom as new Symptom
-				}
-				i++;
-				j=0;
-			}while(i< ListSymptoms.size());
-			
-			/**
-			 * Set up by alphabetical order the list of symptom and return the same array but classify in alphabetic order
-			 * @param List , Comparator
-			 * 
-			 */
-			Collections.sort(Symptom, new Comparator<Object>() {
-				
-				public int compare(Object s1, Object s2) {
-					return((Symptom)s1).getName().compareTo(((Symptom)s2).getName());
-				}
-			});
-			
-			
-			//Test of screen of symptoms
-			int nb=0;
-			
-			if(Symptom != null) {
-				do {
-					Symptom.get(nb).Display();
-					nb++;
-				}while(nb<Symptom.size());
-			}
-			
-		}
-		else {
-			System.out.println("Tableau vide");
-		}
+		
 		
 		/**
 		 *  next generate output
 		 */
-		FileWriter writer = new FileWriter ("result.out");
-		int s=0;
-		do {
-			
-			writer.write("\n"+Symptom.get(s).Write());
-			s++;
-		}while(s< Symptom.size());
-		
-		writer.close();
+	
 	}
 }
